9292721f07a2d5a36c910332e365276b5e445148
Steven Mancine <stevenrmancine@gmail.com>
2021-08-10 22:50:52 -0600

    Add args

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,9 +5,30 @@ import Button from "./Button";
 export default {
   component: Button,
   title: "Components/Button",
+  argTypes: {
+    type: {
+      options: ["primary", "secondary"],
+      control: { type: "radio" },
+      defaultValue: "primary",
+    },
+    round: {
+      options: [true, false],
+      control: { type: "radio" },
+      defaultValue: false,
+    },
+    label: {
+      required: true,
+      control: { type: "text" },
+      defaultValue: "Button",
+    },
+  },
 };
 
-export const Primary = () => <Button>Primary</Button>;
+export const Primary = ({ type, round, label }) => (
+  <Button type={type} round={round}>
+    {label}
+  </Button>
+);
 
 export const Secondary = () => <Button>Secondary</Button>;
 
