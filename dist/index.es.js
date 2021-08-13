4637b6ec7fef8a123a150d6af895bc3307db2b8b
Steven Mancine <stevenrmancine@gmail.com>
2021-08-12 21:39:32 -0600

    Add missing files

---
+++ dist/index.es.js
@@ -0,0 +1,1 @@
+var e=function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(exports,"__esModule",{value:!0}),exports.SayHello=void 0;var o=e(require("./components/HelloWorld"));exports.SayHello=o.default;
