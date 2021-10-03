61ae20a9cbfa31db17321e567b62c6f6e359576b
Steven Mancine <stevenrmancine@gmail.com>
2021-10-03 15:40:02 -0600

    Fix ts errors

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,7 +1,28 @@
 import React, { useState, useEffect } from "react";
+import image1 from "../../assets/images/image1.jpg";
+import image2 from "../../assets/images/image2.jpg";
+import image3 from "../../assets/images/image3.jpg";
 
-const Carousel = () => {
-  return <h1>Carousel</h1>;
+import "./Carousel.scss";
+
+interface ICarousel {
+  images: string[];
+}
+
+const imagesPlaceholders = [image1, image2, image3];
+
+const Carousel = ({ images = imagesPlaceholders }: ICarousel) => {
+  const createSlides = () => {
+    return images.map((image, i) => {
+      return (
+        <div className="carousel__slide" key={i}>
+          <img className="carousel__slide__content" src={image} alt="slide i" />
+        </div>
+      );
+    });
+  };
+
+  return <div className="carousel">{createSlides()}</div>;
 };
 
 export default Carousel;
