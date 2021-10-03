8fe51ea3dbf231994cbebb3edb261aec9cb69e3b
Steven Mancine <stevenrmancine@gmail.com>
2021-10-03 13:49:37 -0600

    Create component

---
+++ src/components/Carousel/Carousel.Stories.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+import Carousel from "./Carousel";
+
+export default {
+  component: Carousel,
+  title: "Components/Carousel",
+  argTypes: {},
+};
+
+export const Primary = () => <Carousel />;
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Carousel = () => {
+  return <h1>Carousel</h1>;
+};
+
+export default Carousel;
