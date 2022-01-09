91b854e14030fca030105b7fe4cd6677a9a1f703
Steven Mancine <stevenrmancine@gmail.com>
2022-01-09 09:43:14 -0700

    Add switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import classnames from "classnames";
 import "./Switch.scss";
 
-const Switch = () => {
-  return <h1>switch</h1>;
+interface ISwitch {
+  className?: string;
+  height?: number;
+  background?: string;
+  ballColor?: string;
+  onClick?: () => void;
+}
+
+const Switch = ({
+  className = "",
+  height = 25,
+  background = "black",
+  ballColor = "white",
+  onClick,
+}: ISwitch) => {
+  const [on, setOn] = useState(false);
+  const width = height * 2;
+  const styles = {
+    width: `${width}px`,
+    height: `${height}px`,
+    outline: `${height * 0.1}px solid ${background}`,
+    background,
+  };
+
+  const ballClass = classnames("switch__ball", {
+    "switch__ball--on": on,
+    "switch__ball--off": !on,
+  });
+
+  const handleClick = () => {
+    onClick && onClick();
+    setOn(!on);
+  };
+  return (
+    <button
+      className={`switch ${className}`}
+      onClick={handleClick}
+      style={styles}
+    >
+      <span className={ballClass} style={{ backgroundColor: ballColor }} />
+    </button>
+  );
 };
 
 export default Switch;
