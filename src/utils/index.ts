44f06afb3e2e8c2df6628ba010b09372d4a29809
Steven Mancine <stevenrmancine@gmail.com>
2022-01-12 22:42:18 -0700

    Fix inex

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 import { calculateTrajectory } from "./calculateTrajectory";
 import random from "./random";
 import range from "./range";
-import preventAnimation from "./preventAnimation";
 
-export { calculateTrajectory, random, range, preventAnimation };
+export { calculateTrajectory, random, range };
