2077c3b382c256611d6ca30e42a5bf50310297a3
Steven Mancine <stevenrmancine@gmail.com>
2022-02-19 09:38:46 -0700

    Change dir

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 import Carousel from "./Carousel";
-import image1 from "../../assets/images/image1.jpg";
-import image2 from "../../assets/images/image2.jpg";
-import image3 from "../../assets/images/image3.jpg";
 
 export default {
   component: Carousel,
@@ -13,8 +10,8 @@ export default {
 
 export const Primary = () => (
   <Carousel>
-    <img src={image1} />
-    <img src={image3} />
-    <img src={image2} />
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
   </Carousel>
 );
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef } from "react";
+import React, { useState, useMemo } from "react";
 import classnames from "classnames";
 
 import { Icon } from "..";
