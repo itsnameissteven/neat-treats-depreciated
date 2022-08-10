c0d8b79ecd82c83a4a9503654cce11923d04e2f9
Steven Mancine <stevenrmancine@gmail.com>
2022-08-09 23:31:21 -0600

    fixes

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import classNames from 'classnames';
-import { useBodyLock } from '../../hooks';
+import React from "react";
+import classNames from "classnames";
+import { useBodyLock } from "../../hooks";
 
 interface IMenu {
   isOpen: boolean;
   children: any;
   width?: string;
   handleClose: () => void;
-  position?: 'left' | 'right';
+  position?: "left" | "right";
   heightFromTop?: string;
   backgroundColor?: string;
   boxShadow?: string;
@@ -19,15 +19,14 @@ const Menu = ({
   isOpen,
   children,
   handleClose,
-  width = '225px',
-  position = 'left',
-  heightFromTop = '50px',
-  backgroundColor = '#fff',
+  width = "225px",
+  position = "left",
+  heightFromTop = "50px",
+  backgroundColor = "#fff",
   boxShadow,
   style = {},
-  containerColor = '#0505051c',
+  containerColor = "#0505051c",
 }: IMenu) => {
-  const [canOpen, setCanOpen] = useState(isOpen);
   useBodyLock(isOpen);
   const styles = {
     width,
@@ -39,8 +38,8 @@ const Menu = ({
 
   const containerClassName = classNames({
     menu: true,
-    'menu-container-enter': isOpen,
-    'menu-container-exit': !isOpen,
+    "menu-container-enter": isOpen,
+    "menu-container-exit": !isOpen,
   });
 
   const slideOutClassName = classNames({
@@ -48,22 +47,6 @@ const Menu = ({
     [`menu__slide-out-enter--${position}`]: isOpen,
     [`menu__slide-out-exit--${position}`]: !isOpen,
   });
-
-  useEffect(() => {
-    const delayOpen = setTimeout(() => {
-      setCanOpen(isOpen);
-    }, 400);
-
-    if (isOpen) {
-      setCanOpen(isOpen);
-    }
-
-    return () => clearTimeout(delayOpen);
-  }, [isOpen]);
-
-  if (!canOpen) {
-    return null;
-  }
 
   return (
     <div
